2c70fd6ed249cdc3829d245199eb568c9f44213d
Fábio Luís <144136133+Falizz@users.noreply.github.com>
2025-09-07 11:47:50 -0300

    Delete JUnit-CadastroPessoas directory

--- JUnit-CadastroPessoas/src/QuantidadeExcedidaException.java
+++
@@ -1,5 +0,0 @@
-public class QuantidadeExcedidaException extends RuntimeException {
-    public QuantidadeExcedidaException(String message) {
-        super("Quantidade de " + message + " excedida");
-    }
-}
